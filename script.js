fd278c8a9e2b8be3cabac11e94e34dfc1cb103a5
allyssonf10 <allysson.nevesf10@gmail.com>
2023-01-28 16:41:31 -0300

    card ajustado

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const cards = document.querySelectorAll('.card');
 
-const handleFlip = (event) => {
-  event.target.parentElement.classList.toggle('flip');
-};
+function handleFlip(){
+    this.classList.toggle('flip');
+}
+
+//const handleFlip = (event) => {
+//  event.target.parentElement.classList.toggle('flip');
+//};
 
 cards.forEach((card) => {
   card.addEventListener('click', handleFlip);
